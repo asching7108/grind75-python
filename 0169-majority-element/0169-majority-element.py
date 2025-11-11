ceafb9fecd3a53b35f304e0d6aa681be64e78908
asching7108 <53709035+asching7108@users.noreply.github.com>
2025-11-11 11:05:02 +0800

    Time: 11 ms (29.17%), Space: 19.3 MB (86.86%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -7,4 +7,9 @@ class Solution:
             counter[i] = counter.get(i, 0) + 1
             if counter[i] > majority:
                 return i
-            
+
+        # counts = collections.Counter(nums)
+        # return max(counts.keys(), key=counts.get)
+
+# Time complexity: O(n)
+# Space complexity: O(n)
