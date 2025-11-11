af520669c887550c0624aa4dee519c34db21dfc7
asching7108 <53709035+asching7108@users.noreply.github.com>
2025-11-11 13:30:19 +0800

    Time: 0 ms (100%), Space: 17.5 MB (95.98%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -4,17 +4,28 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def middleNodeHelper(self, head: ListNode, prevCount: int) -> Tuple[ListNode, int]: # returns (middleNode, nextCount)
-        if not head:
-            return None, 0
-        middleNode, nextCount = self.middleNodeHelper(head.next, prevCount + 1)
-        res = None
-        if middleNode:
-            res = middleNode
-        elif prevCount - nextCount <= 1:
-            res = head
+    # Recursive
+    # def middleNodeHelper(self, head: ListNode, prevCount: int) -> Tuple[ListNode, int]: # returns (middleNode, nextCount)
+    #     if not head:
+    #         return None, 0
+    #     middleNode, nextCount = self.middleNodeHelper(head.next, prevCount + 1)
+    #     res = None
+    #     if middleNode:
+    #         res = middleNode
+    #     elif prevCount - nextCount <= 1:
+    #         res = head
 
-        return [res, nextCount + 1]
+    #     return [res, nextCount + 1]
+
+    # def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    #     return self.middleNodeHelper(head, 0)[0]
 
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        return self.middleNodeHelper(head, 0)[0]
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+# Time complexity: O(n)
+# Space complexity: O(n)
