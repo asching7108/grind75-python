67a06bb67cced700c38940f427e49c96700c0163
asching7108 <53709035+asching7108@users.noreply.github.com>
2025-11-11 13:26:11 +0800

    Time: 0 ms (100%), Space: 17.6 MB (79.75%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNodeHelper(self, head: ListNode, prevCount: int) -> Tuple[ListNode, int]: # returns (middleNode, nextCount)
+        if not head:
+            return None, 0
+        middleNode, nextCount = self.middleNodeHelper(head.next, prevCount + 1)
+        res = None
+        if middleNode:
+            res = middleNode
+        elif prevCount - nextCount <= 1:
+            res = head
+
+        return [res, nextCount + 1]
+
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return self.middleNodeHelper(head, 0)[0]
