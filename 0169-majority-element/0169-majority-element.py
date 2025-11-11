382dd85654886056f33023d237345ce3a7433b9f
asching7108 <53709035+asching7108@users.noreply.github.com>
2025-11-11 11:05:23 +0800

    Time: 7 ms (57.84%), Space: 19.5 MB (18.99%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,15 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        majority = len(nums) // 2
-        counter = {}
+        # majority = len(nums) // 2
+        # counter = {}
 
-        for i in nums:
-            counter[i] = counter.get(i, 0) + 1
-            if counter[i] > majority:
-                return i
+        # for i in nums:
+        #     counter[i] = counter.get(i, 0) + 1
+        #     if counter[i] > majority:
+        #         return i
 
-        # counts = collections.Counter(nums)
-        # return max(counts.keys(), key=counts.get)
+        counts = collections.Counter(nums)
+        return max(counts.keys(), key=counts.get)
 
 # Time complexity: O(n)
 # Space complexity: O(n)
