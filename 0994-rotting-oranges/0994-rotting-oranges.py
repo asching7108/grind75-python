0d07dceb430c12cd15c441c3211f20a08d0b0542
asching7108 <53709035+asching7108@users.noreply.github.com>
2025-11-25 23:00:42 +0800

    Time: 3 ms (81.39%), Space: 17.9 MB (25.76%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -6,12 +6,15 @@ class Solution:
         nr, nc = len(grid), len(grid[0])
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         queue = deque()
-        res = 0
+        minutes_elapsed, fresh_oranges = 0, 0
 
         for r in range(nr):
             for c in range(nc):
+                # Prepare to start multi-source BFS from all rotten oranges
                 if grid[r][c] == 2:
                     queue.append((r, c, 0))
+                elif grid[r][c] == 1:
+                    fresh_oranges += 1
 
         while queue:
             row, col, mins = queue.popleft()
@@ -22,15 +25,11 @@ class Solution:
                     and grid[row + x][col + y] == 1
                 ):
                     grid[row + x][col + y] = 2
+                    fresh_oranges -= 1
                     queue.append((row + x, col + y, mins + 1))
-                    res = mins + 1
+                    minutes_elapsed = mins + 1
 
-        for r in range(nr):
-            for c in range(nc):
-                if grid[r][c] == 1:
-                    return -1
+        return minutes_elapsed if fresh_oranges == 0 else -1
 
-        return res
-
-# Time complexity: O(m * n)
+# Time complexity: O(n)
 # Space complexity: O(min(m, n))
