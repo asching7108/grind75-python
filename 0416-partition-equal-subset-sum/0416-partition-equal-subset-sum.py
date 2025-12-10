1b44dfd9ba2a61aa6b9cfec65da4700bac7a44e4
asching7108 <53709035+asching7108@users.noreply.github.com>
2025-12-10 21:39:17 +0800

    Time: 584 ms (58.06%), Space: 158.3 MB (10.36%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,21 +1,50 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        total = sum(nums)
+        # Bottom-up DP
+        # total = sum(nums)
 
-        if total % 2 != 0:
-            return False
+        # # Total must be even
+        # if total % 2 != 0:
+        #     return False
 
-        target = total // 2
+        # # We need to find a subset that sums up to target
+        # target = total // 2
 
-        dp = [False] * (target + 1)
-        dp[0] = True
+        # dp = [False] * (target + 1)
+        # dp[0] = True
 
-        for num in nums:
-            for i in range(target, num - 1, -1):
-                if dp[i - num]:
-                    dp[i] = True
+        # for num in nums:
+        #     for i in range(target, num - 1, -1):
+        #         if dp[i - num]:
+        #             dp[i] = True
 
-        return dp[target]
+        # return dp[target]
 
         # Time complexity: O(m * n) where m is the target and n is the length of nums
         # Space complexity: O(m)
+
+        # Top-down DP
+        total = sum(nums)
+
+        # Total must be even
+        if total % 2 != 0:
+            return False
+
+        # We need to find a subset that sums up to target
+        target = total // 2
+
+        @lru_cache(maxsize=None)
+        def dfs(i: int, curr: int) -> bool:
+            if curr == target:
+                return True
+            
+            if curr > target or i == len(nums):
+                return False
+
+            # nums[i] is either included or not included in the subset
+            return dfs(i + 1, curr + nums[i]) or dfs(i + 1, curr)
+
+        return dfs(0, 0)
+
+        # Time complexity: O(m * n) where m is the target and n is the length of nums
+        # Space complexity: O(m * n), O(m * n) for memoization and O(n) for recursive call stack
