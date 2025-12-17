08204103198cef581cd0be18a27bb4acd30437ef
asching7108 <53709035+asching7108@users.noreply.github.com>
2025-12-17 17:33:09 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,29 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m, n = len(board), len(board[0])
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+        def dfs(row, col, path):
+            print(path + [(row,col)])
+            if len(path) == len(word):
+                return True
+
+            res = False
+            if (0 <= row < m
+                and 0 <= col < n
+                and (row, col) not in path
+                and board[row][col] == word[len(path)]
+            ):
+                path.append((row, col))
+                for y, x in directions:
+                    if dfs(row + y, col + x, path):
+                        return True
+
+            return False
+            
+        for row in range(m):
+            for col in range(n):
+                if dfs(row, col, []):
+                    return True
+
+        return False
