17386d3d1a3276d6be1cb1b62883207f7bbeb344
asching7108 <53709035+asching7108@users.noreply.github.com>
2025-12-17 11:15:10 +0800

    Time: 0 ms (100%), Space: 17.9 MB (41.15%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -2,14 +2,14 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         # Map all the digits to corresponding letters
         letters = {
-            "2": "abc",
-            "3": "def",
-            "4": "ghi",
-            "5": "jkl",
-            "6": "mno",
-            "7": "pqrs",
-            "8": "tuv",
-            "9": "wxyz",
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz',
         }
         results = []
 
