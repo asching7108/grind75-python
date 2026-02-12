6ecb653c433c8739d682d483e14f261de107f2a1
asching7108 <53709035+asching7108@users.noreply.github.com>
2026-02-12 11:48:08 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.py
+++ 1114-print-in-order/1114-print-in-order.py
@@ -1,33 +1,20 @@
-from threading import Lock
-
-
 class Foo:
     def __init__(self):
-        self.firstJobDone = Lock()
-        self.secondJobDone = Lock()
-        self.firstJobDone.acquire()
-        self.secondJobDone.acquire()
+        pass
 
 
     def first(self, printFirst: 'Callable[[], None]') -> None:
         
-        # printFirst() outputs "first". Do not change or remove this line.
         printFirst()
-
-        self.firstJobDone.release()
 
 
     def second(self, printSecond: 'Callable[[], None]') -> None:
         
-        with self.firstJobDone:
-            # printSecond() outputs "second". Do not change or remove this line.
-            printSecond()
-
-            self.secondJobDone.release()
+        # printSecond() outputs "second". Do not change or remove this line.
+        printSecond()
 
 
     def third(self, printThird: 'Callable[[], None]') -> None:
         
-        with self.secondJobDone:
-            # printThird() outputs "third". Do not change or remove this line.
-            printThird()
+        # printThird() outputs "third". Do not change or remove this line.
+        printThird()
